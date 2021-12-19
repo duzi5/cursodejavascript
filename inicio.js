924fbefa4147180b58a73b31129f8d04a25fa0d4
Antonio Netto <60482472+duzisnetto@users.noreply.github.com>
2021-12-19 19:05:34 -0300

    iniciando a super revisão

---
+++ inicio.js
@@ -0,0 +1,9 @@
+console.log('iniciando os trabalhos')
+
+
+// Dentro do javaescript existe uma diferença entre a execução de um script pelo node 
+// ou pelo navegador. o objeto global tem esse mesmo nome no node, no entanto no 
+// navegador, o mesmo objeto se chama window.
+
+
+// O let cria a variável no escopo local enquando o var cria no objeto global. 
