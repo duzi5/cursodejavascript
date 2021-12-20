beee196d44e8b15ea50bbf07e2ab7612e3b5f0b5
Antonio Netto <60482472+duzisnetto@users.noreply.github.com>
2021-12-20 16:56:41 -0300

    estudando funções

---
+++ functions.js
@@ -0,0 +1,21 @@
+//é possível passar uma função como um membro de um array
+//a função é uma cidadã de primeira classe para o javascript
+//arrow function tem um tratamento diferente para o this, pois não cria um novo escopo {}
+//
+
+
+function custodeproduao(){
+    const custoTotal = function(custodoVidro, personalizacaodoVidro){
+        var custodoVidro = null     
+        var personalizacaodoVidro = null
+
+
+        return custodoVidro + personalizacaodoVidro
+        
+        
+       
+    }
+    
+}
+
+
